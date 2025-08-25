1e7e0a91822e3e052ce66bf99b597439e06c4acc
BhagyashreeKale <bhagyshreekale10@gmail.com>
2025-08-25 17:16:32 +0530

    Added 1st Component

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,18 @@
-import React, { useState } from 'react'
+import React, { useState } from 'react';
+import Hello from './components/Hello';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <div style={{ textAlign: 'center', marginTop: '50px' }}>
       <h1>Hello React + TypeScript + Vite!</h1>
+      <Hello name="Developer" />   {/* 👈 Using new component */}
       <p>Simple Counter Example</p>
       <h2>{count}</h2>
       <button onClick={() => setCount(count + 1)}>Increment</button>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
